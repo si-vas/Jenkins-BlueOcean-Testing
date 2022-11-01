3fbdcf400c3c291babb55051fa323509da513c01
si-vas <99237926+si-vas@users.noreply.github.com>
2022-11-01 02:04:20 -0400

    Sleep_Time Added

--- ec2.tf
+++ ec2.tf
@@ -14,3 +14,7 @@ apt update -y
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC5Ar8YsSFS0uSDcvmcKPWFQJgZ5pXZbxs+p+k4VTsFiTQ1VDakjWPYxyPcH33Ejxigfgo65YICO9jBxGXUppeHRKQQLOH3vsiqNrGCdG6DajnBqROkWg3eiQAuLL9t8185tRndgeouTS3cc/uXVUsYYKu2TG0N6wcsYO20dkVg8cI3xKgIfpbO5Ufh6gMqFbFjxttmJOaXmYD5P0vkqJXmfUMJnJ8uxqRo1OyoBWL/+nlgmBxf6baFZPPk5mgmdDRL13X1R2LE89Jyptw1mTcSfF1nYjSUTM+PvTAJllUSW+9qsa5S1Q2eN9kBgsJRVDZq8uTnV2NkyiZyP1/02LN/uSpCQrK/BeZ/3HcBifQjPs1iyk0Hs6OzzYwGcwgCMZcg8b1mdFgXTFnjC1TwJi+HQGUphcTpKSCm4u8WlBOeIx8ZWZAEGxYqyDSJZKg1gpNnorYW8Ip6PKk1ySgATR93yvJlYeqNNZMZgglwPNSIW4gZZpmbl7sCs959+t3ZyVE= siva@MLBPBCL-106780' >> /home/ubuntu/.ssh/authorized_keys
 EOF
 }
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [aws_instance.web]
+  create_duration = "30s"
+}
