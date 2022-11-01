4908412060678e919d813b7e58bc706f35022598
si-vas <99237926+si-vas@users.noreply.github.com>
2022-11-01 06:23:31 -0400

    S3 Backend Updated

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-#terraform {
-# backend "s3" {
-#    bucket = "terraformbucket"
-#    key    = "myterraform.tfstate"
-#    region = "ap-south-1"
-# }
-#}
+terraform {
+ backend "s3" {
+    bucket = "terraformbucket"
+    key    = "dev.terraform.tfstate"
+    region = "ap-south-1"
+ }
+}
 
 resource "aws_vpc" "default" {
   cidr_block           = var.vpc_cidr
